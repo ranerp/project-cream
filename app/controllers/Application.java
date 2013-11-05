20dc67ae5a57a63739b950cbcd7935d000a64501
twtChnz <raner.piibur@gmail.com>
2013-11-05 14:59:57 +0200

    First Controller index

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,35 +1,14 @@
 package controllers;
 
 import play.*;
-import play.libs.F;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
 
-    public static WebSocket<String> index() {
-        return new WebSocket<String>() {
-
-            @Override
-            public void onReady(WebSocket.In<String> stringIn, WebSocket.Out<String> stringOut) {
-                stringIn.onMessage(new F.Callback<String>() {
-                    @Override
-                    public void invoke(String s) throws Throwable {
-                        System.out.println(s);
-                    }
-                });
-
-                stringIn.onClose(new F.Callback0() {
-                    @Override
-                    public void invoke() throws Throwable {
-                        System.out.println("Disconnected");
-                    }
-                });
-
-                stringOut.write("Hello");
-            }
-        };
+    public static Result index() {
+        return ok(index.render("Your new application is ready."));
     }
 
 }
