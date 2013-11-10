0f5304159b6f7aa356b046d1d441f5d2ef7449c6
twtChnz <raner.piibur@gmail.com>
2013-11-10 18:36:45 +0200

    modifications

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,55 +1,26 @@
 package controllers;
 
+import models.ImageReceiver;
 import play.*;
-import play.libs.F;
 import play.mvc.*;
-
-import views.html.*;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-
 
 public class Application extends Controller {
 
-    public static WebSocket<byte[]> index() {
 
+    public static Result index() {
+        return ok("It Works!");
+    }
+
+    public static WebSocket<byte[]> receiveImage() {
         return new WebSocket<byte[]>() {
 
             @Override
             public void onReady(WebSocket.In<byte[]> in, WebSocket.Out<byte[]> out) {
-                in.onMessage(new F.Callback<byte[]>() {
-                    @Override
-                    public void invoke(byte[] inPayload) throws Throwable {
-                        System.out.println("TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST");
-                        byte[] header = new byte[256];
-                        for(int i = 0; i < header.length; i++) {
-                            header[i] = inPayload[i];
-                        }
-
-                        String headerStr = new String(header, "UTF-8");
-                        byte[] imgBytes = new byte[inPayload.length - header.length];
-                        for(int i = 0; i < inPayload.length; i++) {
-                            imgBytes[0] = inPayload[i + header.length];
-                        }
-
-                        BufferedImage img = ImageIO.read(new ByteArrayInputStream(imgBytes));
-                        System.out.println(headerStr);
-                    }
-                });
-
-                in.onClose(new F.Callback0() {
-                    @Override
-                    public void invoke() throws Throwable {
-                        System.out.println("Disconnected");
-                    }
-                });
-
-                String str = "Hello android";
-
-                out.write(str.getBytes());
-
+                try {
+                    ImageReceiver.start(in, out);
+                } catch (Exception e) {
+                    Logger.debug(e.toString());
+                }
             }
         };
     }
