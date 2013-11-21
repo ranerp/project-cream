8acca65b9b9dc3e27c5dbd4f0fa2b845ff5315d5
twtChnz <raner.piibur@gmail.com>
2013-11-21 23:52:59 +0200

    fixes

---
+++ app/plugins/S3Plugin.java
@@ -0,0 +1,11 @@
+package plugins;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: twtChnz
+ * Date: 10.11.13
+ * Time: 21:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class S3Plugin {
+}
